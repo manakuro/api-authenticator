35d5c6b00e6b85d826915fa307579dc3ed1091ad
manato kuroda <a7108100@gmail.com>
2018-12-29 08:56:09 +0900

    fix: add github to omniauth_provider in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
-         :validatable, :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :validatable, :confirmable, :omniauthable, omniauth_providers: %i[google_oauth2 github]
 
   include DeviseTokenAuth::Concerns::User
 end
